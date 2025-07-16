b5b1d08de2683fee8b408bbfd556a872399006e6
Lakshit Raj <raj.lakshit12@gmail.com>
2025-07-17 03:08:50 +0530

    Delete Components directory

--- Components/HeroBanner.jsx
+++
@@ -1,8 +0,0 @@
-export default function HeroBanner() {
-  return (
-    <div className="hero">
-      <h1>🎬 Welcome to DevFlix</h1>
-      <p>Your tech creations. Binge-worthy brilliance.</p>
-    </div>
-  );
-}
