d7dcb85fd68f7a450106bd2cff21f52fc9dbcc90
Lakshit Raj <raj.lakshit12@gmail.com>
2025-07-17 02:50:31 +0530

    Add files via upload

---
+++ RajFlix-Portfolio/project.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Weather Watch",
+    tech: ["JavaScript", "HTML", "CSS", "WeatherAPI"],
+    thumbnail: "/thumbnails/weather-watch.png",
+    description: "Displays real-time city weather with sleek API integration.",
+    liveDemo: "https://lakshitraj-io.github.io/weather-app-LakshitRaj/",
+  },
+  {
+    title: "Code-Wiz Chronicles",
+    tech: ["JavaScript", "HTML", "CSS"],
+    thumbnail: "/thumbnails/code-wiz.png",
+    description: "Login-protected site for a coding club with full functionality.",
+    liveDemo: "https://lakshitraj-io.github.io/Code-Wiz-Club-Website/",
+  },
+  {
+    title: "The To-Do Trials",
+    tech: ["JavaScript", "HTML", "CSS"],
+    thumbnail: "/thumbnails/todo-trials.png",
+    description: "Create, strike, or remove tasks with a minimalist interface.",
+    liveDemo: "https://lakshitraj-io.github.io/To-Do-List-By-LakshitRaj/",
+  },
+  {
+    title: "Recipe Rumble",
+    tech: ["Python", "Streamlit", "TheMealDB API"],
+    thumbnail: "/thumbnails/recipe-rumble.png",
+    description: "AI-powered recipe finder using open-source API hosted on Streamlit.",
+    liveDemo: "https://ai-powered-recipie-app.streamlit.app",
+  },
+  {
+    title: "Student Records: Enrolled",
+    tech: ["C#", "WinForms"],
+    thumbnail: "/thumbnails/student-db.png",
+    description: "Manage student data with roll numbers and course assignment.",
+    liveDemo: "https://lakshitraj-io.github.io/Student-Management-System/",
+  },
+  {
+    title: "Library Vault",
+    tech: ["C#", "WinForms"],
+    thumbnail: "/thumbnails/library-db.png",
+    description: "Efficiently manage library book data and inventory tracking.",
+    liveDemo: "https://lakshitraj-io.github.io/LibraryManagementSystem/",
+  },
+];
